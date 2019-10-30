0ddf7130735a85f5398909fa3a7f4ad9ae38d70e
Bakanagoku <45848951+Bakanagoku@users.noreply.github.com>
2019-10-30 10:14:46 +0100

    Update main.c

--- main.c
+++ main.c
@@ -4,11 +4,25 @@
  *
  * Created on 15. Oktober 2019, 15:33
  */
+#include <stdbool.h>
+
+
+
+int ADC_ReadPercentage();
+int ADC_Read10bit();
+bool ADC_ChannelEnable();
+bool ADC_SetConfiguration();
+float mapping(float,float,float, bool);
+int get_red(float);
+int get_green(float);
+int get_blue(float);
+float normalize(int);
 
 
 #include <xc.h>
 #include <stdio.h>
-#include <stdbool.h>
+#include <math.h>
+
 
 #define FCY 1600000UL // Wird benötigt, damit __delay_ms() funktioniert.
 //#define _XTAL_FREQ 4000000
@@ -21,6 +35,10 @@
 
 void main(void) {
     
+    uint16_t potentiometer;
+    
+    ADC_SetConfiguration();
+    ADC_ChannelEnable();
     
     TRISAbits.TRISA0 = 0;
 
@@ -29,30 +47,29 @@ void main(void) {
     TRISCbits.TRISC7 = 0;
     
     TRISBbits.TRISB12 = 0;
-   
-
-    Nop();
-
     
-
-    int rW, rD, gW, gD, bW, bD;
     
-   
+    int rV, rD, gV, gD, bV, bD, current_color;
+    
+    
+    
+    
+    
 
     while (1){
         
+        potentiometer = ADC_Read10bit();
         
+        rV = get_red(potentiometer);
         
-        rW = 1;
+        gV = get_green(potentiometer);
         
-        gW = 1;
-        
-        bW = 1;
+        bV = get_blue(potentiometer);
         
         int j;
         for (j=1;j<=255;j++){
             
-            if (j>rW){
+            if (j>rV){
 
                 PORTAbits.RA0 = 0; //Rot
                 
@@ -61,7 +78,7 @@ void main(void) {
                 PORTAbits.RA0 = 1; //Rot
             }
 
-            if (j>gW){
+            if (j>gV){
 
                 PORTAbits.RA1 = 0; //Grün
                 
@@ -72,7 +89,7 @@ void main(void) {
 
             }               
 
-            if (j>bW){
+            if (j>bV){
 
                 PORTCbits.RC7 = 0; //Blau
                 
@@ -82,10 +99,7 @@ void main(void) {
                 PORTCbits.RC7 = 1; //Blau
 
             }
-
         }
-
-
     }
 
    
@@ -96,30 +110,72 @@ void main(void) {
 
 }
 
-int map_color_scheme() {
-    
-    
-    
-    
+
+float normalize(int poti) {
+    float xOne = pow(57,4.0/5.0);
+    return (float)pow(poti,1.0/5.0) * xOne;
 }
 
-uint8_t ADC_ReadPercentage() {
-    uint8_t percent ;
-
-    
-
-    //A very crude percentage calculation
-    percent = ( ADC_Read10bit ( 8 ) / 10 ) ;
-
-    if (percent > 100)
-    {
-        percent = 100 ;
+float mapping(float value, float start1, float stop1, bool test) {
+    int rgb_min;
+    int rgb_max;
+    if(test) {
+        rgb_min = 0.0;
+        rgb_max = 255.0;
+    } else {
+        rgb_min = 255.0;
+        rgb_max = 0.0;
     }
-    return percent ;
+    if((stop1 - start1) == 0 ) {
+        return 0.0;
+    } else {
+        return (rgb_min + (rgb_max - rgb_min) * ((value - start1) / (stop1 - start1)));
+    }
 }
 
-uint16_t ADC_Read10bit () {
-    uint16_t i ;
+int get_red(float pv) {
+    int red_value;
+    if(pv <= 10) {
+        return 255;
+    } else if(pv <= 20) {
+        return mapping(pv, 10, 20, false);
+    } else if(pv <= 40) {
+        return 0;
+    } else if(pv <= 50) {
+        return mapping(pv, 40, 50, true);
+    } else {
+        return 255;
+    }
+}
+
+int get_green(float pv) {
+    int green_value;
+    if(pv <= 10) {
+        return mapping(pv, 0, 10, true);
+    } else if(pv <= 30) {
+        return 50;
+    } else if(pv <= 40) {
+        return mapping(pv, 30, 40, false);
+    } else {
+        return 0;
+    }
+}
+
+int get_blue(float pv) {
+    int blue_value;
+    if(pv <= 20) {
+        return 0;
+    } else if(pv <= 30) {
+        return mapping(pv, 20, 30, true);
+    } else if(pv <= 50) {
+        return 255;
+    } else {
+        return mapping(pv, 50, 60, false);
+    }
+}
+
+int ADC_Read10bit () {
+    int i ;
 
     AD1CHS = 8;
 
@@ -132,15 +188,30 @@ uint16_t ADC_Read10bit () {
 
     while (!AD1CON1bits.DONE) ;       //Wait for conversion to complete
 
-    return ADC1BUF0 ;
+    return ADC1BUF0;
 }
 
-bool ADC_ChannelEnable () {
+int ADC_ReadPercentage() {
+    int percent ;
+
+    //A very crude percentage calculation
+    percent = ( ADC_Read10bit ( 8 ) / 10 ) ;
+
+    if (percent > 100)
+    {
+        percent = 100 ;
+    }
+    return percent ;
+}
+
+
+
+bool ADC_ChannelEnable() {
     ANSCbits.ANSC0 = 1;
     return true ;
     }
 
-bool ADC_SetConfiguration () {
+bool ADC_SetConfiguration() {
 
     AD1CON2bits.PVCFG = 0x00 ;
     AD1CON2bits.NVCFG0 = 0x0 ;
@@ -153,4 +224,24 @@ bool ADC_SetConfiguration () {
     AD1CON1bits.ADON = 1 ;
 
     return true ;
+}
+
+void blink_amount(int amount) {
+    PORTAbits.RA0 = 0;
+    PORTAbits.RA1 = 0;
+    PORTCbits.RC7 = 0;
+    int i;
+    __delay_ms(500);
+    
+    for(i = 0; i < amount; i++) {
+        PORTCbits.RC7 = 1;
+        __delay_ms(500);
+        PORTCbits.RC7 = 0;
+        __delay_ms(500);
+    }
+}
+
+
+int root(int input, int n) {
+  return (int)pow(input, 1./n);
 }
